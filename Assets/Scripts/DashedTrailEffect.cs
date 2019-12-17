b71d3ecb9c6d767986be0c4ddc55dda1f546e39e
Alden <aldensmith06@Gmail.com>
2019-12-16 23:37:18 -0800

    Figured out animations!
    Stuffs going to start looking pretty!

---
+++ Assets/Scripts/DashedTrailEffect.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashedTrailEffect : MonoBehaviour
+{
+    float timeBtwSpawns;
+    public float startTimeBtwSpawns;
+    public float deleteAfterSeconds;
+
+    public GameObject dash;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeBtwSpawns <= 0)
+        {
+            GameObject line = Instantiate(dash, transform.position, transform.rotation);
+            Destroy(line, deleteAfterSeconds);
+            timeBtwSpawns = startTimeBtwSpawns;
+        }
+        else
+        {
+            timeBtwSpawns -= Time.deltaTime;
+        }
+        
+    }
+}
