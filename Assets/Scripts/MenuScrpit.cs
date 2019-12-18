35117bcb489cef8dffff19c395cfc917d2dfe69e
Alden <aldensmith06@Gmail.com>
2019-12-18 14:03:55 -0800

    Menu Animatoins almost finished
    Finish credits and moving on to main game & pause animations

--- Assets/Scripts/MenuScrpit.cs
+++ Assets/Scripts/MenuScrpit.cs
@@ -7,10 +7,34 @@ public class MenuScrpit : MonoBehaviour
 {
 
     public GameObject creditPanel;
+
+    public GameObject title;
+    Animator titleAnimator;
+
+    public GameObject ball;
+    Animator ballAnimator;
+
+    public GameObject play;
+    Animator playAnimator;
+
+    public GameObject credits;
+    Animator creditsAnimator;
+
+    public GameObject quit;
+    Animator quitAnimator;
+
+    void Start()
+    {
+        titleAnimator = title.GetComponent<Animator>();
+        ballAnimator = ball.GetComponent<Animator>();
+        playAnimator = play.GetComponent<Animator>();
+        creditsAnimator = credits.GetComponent<Animator>();
+        quitAnimator = quit.GetComponent<Animator>();
+    }
     public void Play()
     {
         Debug.Log("Playing...");
-        SceneManager.LoadScene("GameScene");
+        StartCoroutine("WaitAndLoad");
     }
 
     public void Quit()
@@ -31,5 +55,14 @@ public class MenuScrpit : MonoBehaviour
         creditPanel.SetActive(false);
     }
 
-
+    IEnumerator WaitAndLoad()
+    {
+        titleAnimator.SetBool("InMenu", false);
+        ballAnimator.SetBool("InMenu", false);
+        playAnimator.SetBool("InMenu", false);
+        creditsAnimator.SetBool("InMenu", false);
+        quitAnimator.SetBool("InMenu", false);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("GameScene");
+    }
 }
